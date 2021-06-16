28dbeb13e09ffef20370cf806f1c17d0e906c16a
Hyochan Park <hyopark@c3r3s7.42seoul.kr>
2021-06-16 16:52:41 +0900

    diretory rmove

--- execute/srcs/parsing_util.c
+++ execute/srcs/parsing_util.c
@@ -3,14 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_util.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chanykim <chanykim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hyopark <hyopark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 17:33:11 by chanykim          #+#    #+#             */
-/*   Updated: 2021/06/15 16:50:19 by chanykim         ###   ########.fr       */
+/*   Updated: 2021/06/16 16:48:50 by hyopark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_parsing.h"
+
+int		check_red(char *buf, int i, int len)
+{
+	while (i < len)
+	{
+		if (is_inquote(buf, i))
+		{
+			i++;
+			continue ;
+		}
+		if (buf[i] != '\0' && (buf[i] == '>' || buf[i] == '<'))
+		{
+			if (buf[i + 1] != '\0' && (buf[i + 1] == '>' || buf[i + 1] == '<'))
+			{
+				if ((buf[i] == '>' && buf[i + 1] == '>'))
+				{
+					if (buf[i + 2] != '\0' && buf[i + 2] == '>')
+					{
+						return (0 * printf("red error\n"));
+					}
+				}
+				else
+					return (0 * printf("red error\n"));
+			}
+		}
+		i++;
+	}
+	return (1);
+}
 
 char	*remove_empty_red(char *buf)
 {
@@ -47,6 +76,8 @@ int		pre_parsing(char *buf)
 	if (!buf || !check_quote(buf, 0, 0, 0))
 		return (printf("error : match quote\n") * 0);
 	if (!buf || check_syntax(buf, 0, ft_strlen(buf) - 1, 0) == 0)
+		return (0);
+	if (!buf || check_red(buf, 0, ft_strlen(buf) - 1) == 0)
 		return (0);
 	buf = remove_escape(buf, 0);
 	if (!buf)
--- func_test/ls.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
-
